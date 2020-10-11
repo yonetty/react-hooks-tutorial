6509f4e3ab4aacd4fe58ae0918c98be85cd3d68d
yonetty <tyonekubo@outlook.jp>
2020-10-11 21:08:05 +0900

    step6 custom Hooks

--- src/BookSearchDialog.tsx
+++ src/BookSearchDialog.tsx
@@ -1,30 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { BookDescription } from './BookDescription';
 import BookSearchItem from './BookSearchItem';
-
-function extractBooks(json: any): BookDescription[] {
-  const items: any[] = json.items;
-  return items.map((item: any) => {
-    const volumeInfo: any = item.volumeInfo;
-    return {
-      title: volumeInfo.title,
-      authors: volumeInfo.authors ? volumeInfo.authors.join(', ') : "",
-      thumbnail: volumeInfo.imageLinks ? volumeInfo.imageLinks.smallThumbnail : "",
-    }
-  });
-}
-
-function buildSearchUrl(title: string, author: string, maxResults: number): string {
-  let url = "https://www.googleapis.com/books/v1/volumes?q=";
-  const conditions: string[] = []
-  if (title) {
-    conditions.push(`intitle:${title}`);
-  }
-  if (author) {
-    conditions.push(`inauthor:${author}`);
-  }
-  return url + conditions.join('+') + `&maxResults=${maxResults}`;
-}
+import useBookData from './useBookData';
 
 type BookSearchDialogProps = {
   maxResults: number;
@@ -32,35 +9,19 @@ type BookSearchDialogProps = {
 }
 
 const BookSearchDialog = (props: BookSearchDialogProps) => {
-  const [books, setBooks] = useState([] as BookDescription[]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
   const titleRef = useRef<HTMLInputElement>(null);
   const authorRef = useRef<HTMLInputElement>(null);
-  const [isSearching, setIsSearching] = useState(false);
-
-  useEffect(() => {
-    if (isSearching) {
-      const url = buildSearchUrl(titleRef.current!.value, authorRef.current!.value, props.maxResults);
-      fetch(url)
-        .then((res) => {
-          return res.json();
-        })
-        .then((json) => {
-          return extractBooks(json);
-        }).then((books) => {
-          setBooks(books);
-        }).catch((err) => {
-          console.error(err);
-        });
-    }
-    setIsSearching(false);
-  }, [isSearching]);
+  const books = useBookData(title, author, props.maxResults);
 
   const handleSearchClick = () => {
     if (!titleRef.current!.value && !authorRef.current!.value) {
       alert("条件を入力してください");
       return;
     }
-    setIsSearching(true);
+    titleRef.current && setTitle(titleRef.current!.value);
+    authorRef.current && setAuthor(authorRef.current!.value);
   }
 
   const handleBookAdd = (book: BookDescription) => {
---
+++ src/useBookData.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { BookDescription } from './BookDescription';
+
+function extractBooks(json: any): BookDescription[] {
+  const items: any[] = json.items;
+  return items.map((item: any) => {
+    const volumeInfo: any = item.volumeInfo;
+    return {
+      title: volumeInfo.title,
+      authors: volumeInfo.authors ? volumeInfo.authors.join(', ') : "",
+      thumbnail: volumeInfo.imageLinks ? volumeInfo.imageLinks.smallThumbnail : "",
+    }
+  });
+}
+
+function buildSearchUrl(title: string, author: string, maxResults: number): string {
+  let url = "https://www.googleapis.com/books/v1/volumes?q=";
+  const conditions: string[] = []
+  if (title) {
+    conditions.push(`intitle:${title}`);
+  }
+  if (author) {
+    conditions.push(`inauthor:${author}`);
+  }
+  return url + conditions.join('+') + `&maxResults=${maxResults}`;
+}
+
+const useBookData = (title: string, author: string, maxResults: number) => {
+  const [books, setBooks] = useState([] as BookDescription[]);
+
+  useEffect(() => {
+    if (title || author) {
+      const url = buildSearchUrl(title, author, maxResults);
+      fetch(url)
+        .then((res) => {
+          return res.json();
+        })
+        .then((json) => {
+          return extractBooks(json);
+        }).then((books) => {
+          setBooks(books);
+        }).catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [title, author, maxResults]);
+
+  return books;
+}
+
+export default useBookData;
